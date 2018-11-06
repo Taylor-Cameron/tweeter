cb06907d83db6bfb656822349ab2e5f90c25dd1b
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-11-06 21:04:10 +0000

    character counter and jquery configuration

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  var counter = parseInt($('.counter')[0].innerHTML);
+  document.addEventListener('keyup', function(event) {
+    var textCount = $('#text')[0].textLength;
+    if(textCount > 140) {
+      $('.counter').addClass('negative');
+    } else if(textCount < 140) {
+      $('.counter').removeClass('negative');
+    }
+    if(counter < 141) {
+      $('.counter')[0].innerHTML = counter - textCount;
+    }
+  });
+});
