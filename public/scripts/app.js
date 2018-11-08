2706477759f5a3953a3284fdf976d4929c4f80d3
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-11-08 23:22:22 +0000

    mongobd added and replace hardcoded db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = [
-  {
-    'user': {
-      'name': 'Newton',
-      'avatars': {
-        'small': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
-        'regular': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
-        'large': 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png'
-      },
-      'handle': '@SirIsaac'
-    },
-    'content': {
-      'text': 'If I have seen further it is by standing on the shoulders of giants'
-    },
-    'created_at': 1461116232227
-  },
-  {
-    'user': {
-      'name': 'Descartes',
-      'avatars': {
-        'small': 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png',
-        'regular': 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png',
-        'large': 'https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png'
-      },
-      'handle': '@rd'
-    },
-    'content': {
-      'text': 'Je pense , donc je suis'
-    },
-    'created_at': 1461113959088
-  },
-  {
-    'user': {
-      'name': 'Johann von Goethe',
-      'avatars': {
-        'small': 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png',
-        'regular': 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png',
-        'large': 'https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png'
-      },
-      'handle': '@johann49'
-    },
-    'content': {
-      'text': 'Es ist nichts schrecklicher als eine tätige Unwissenheit.'
-    },
-    'created_at': 1461113796368
-  }
-];
-
 $(document).ready(function () {
 
   function createTweetElement (tweets) {
@@ -78,15 +30,6 @@ $(document).ready(function () {
           .text(moment(tweets.created_at).fromNow())));
     return $tweet;
   }
-
-  function renderTweet(tweets) {
-    tweets.forEach(function(obj) {
-      let $tweet = createTweetElement(obj);
-      $('#tweets-container').append($tweet);
-    });
-  }
-
-  renderTweet(tweetData);
 
   function tweetSubmitted() {
     var $button = $('#submit');
@@ -120,9 +63,13 @@ $(document).ready(function () {
   function loadTweets() {
     $.ajax('/tweets', { method: 'get'})
       .then(function(tweets) {
-        $('#tweets-container').prepend(createTweetElement(tweets.pop()));
+        tweets.forEach(function (obj) {
+          let $tweet = createTweetElement(obj);
+          $('#tweets-container').prepend($tweet);
+        });
       });
   }
+  loadTweets();
 
   $('.compose').click(function () {
     $('.new-tweet').toggle('fast');
